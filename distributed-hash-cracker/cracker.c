80addcf920a5caea96ff27da2f8f8fd94af54376
younix <web2p10@wemelug.de>
2009-10-15 01:19:18 +0200

    add next key function

--- distributed-hash-cracker/cracker.c
+++ distributed-hash-cracker/cracker.c
@@ -7,6 +7,7 @@
 #define MD5	1
 #define SHA1	2
 
+// Define a cracing task
 typedef struct {
 	int base;
 	int keysize;
@@ -19,12 +20,8 @@ typedef struct {
 	char* end_key;
 } crack_task;
 
-typedef struct {
-	char* start_key;
-	char* end_key;
-} task_split;
-
-int keynr_2_key( crack_task, int, char*);
+int keynr_2_key(crack_task, int, char*);
+int get_next_key(crack_task, char*, int);
 
 int main( int argc, char **argv)
 {
@@ -36,15 +33,23 @@ int main( int argc, char **argv)
 	crack.base = 3;
 	crack.keysize = 6;
 
-	key = (char*) calloc(sizeof(char), max_keysize);
-	crack.charset = (char*) calloc(sizeof(char), base);
+	key = (char*) calloc(sizeof(char), crack.keysize);
+	crack.charset = (char*) calloc(sizeof(char), crack.base);
 
 	strncpy( crack.charset, "abc", crack.base);
+
+	while(get_next_key(crack, key, 0) != -1)
+		printf("%s\n", key);
 
 	keynr_2_key(crack, key_nr, key);
 
 	printf("Keynumber: %d key: %s\n", key_nr, key);
 
+	get_next_key(crack, key, 0);
+
+	printf("next key: %s\n", key);
+
+	
 	free(key);
 
 	return 0;
@@ -65,3 +70,34 @@ int keynr_2_key( crack_task crack, int key_nr, char *key)
 
 	return 0;
 }
+
+//increas the key by one
+int get_next_key(crack_task crack, char* key, int pos)
+{
+	int i = 0;
+	
+	for(i = 0; i < crack.base; ++i)
+	{
+		if(key[pos] == crack.charset[i])
+		{
+			if((i + 1) < crack.base)
+			{
+				key[pos] = crack.charset[i+1];
+				return 0;
+			}
+			else
+			{
+				key[pos] = crack.charset[0];
+				if((pos + 1) < crack.keysize)
+					return get_next_key(crack, key, pos + 1);
+				else
+					return -1;;
+			}
+		}
+	}
+
+	key[pos] = crack.charset[0];
+
+	return 0;
+}
+
