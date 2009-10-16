4d9ed23a07b454dec8c2b134a74ffea765ac7e99
younix <web2p10@wemelug.de>
2009-10-16 18:39:02 +0200

    splitt the source code in the header and the implementation part

--- distributed-hash-cracker/cracker.c
+++ distributed-hash-cracker/cracker.c
@@ -4,31 +4,7 @@
 #include <stdlib.h>
 #include <crypt.h>
 
-#define MD5	1
-#define SHA1	2
-
-// Define a cracing task
-typedef struct {
-	int base;
-	int keysize_max;
-	char* charset;
-	char* hash;
-	char* salt;
-	int algorithm;
-
-	//additional information
-	unsigned long long int keyrange;
-
-	// for splitting calculation, e.g. threading or distributed computing
-	char* start_key;
-	char* end_key;
-} crack_task;
-
-unsigned long long int keyrange(crack_task);
-int keynr_2_key(crack_task, int, char*);
-int get_next_key(crack_task, char*, int);
-int ben_next_key(crack_task, char*);
-int compare_hash(char*, char*);
+#include "cracker.h"
 
 int main( int argc, char **argv)
 {
--- distributed-hash-cracker/cracker.h
+++ distributed-hash-cracker/cracker.h
@@ -1,1 +1,36 @@
+#ifndef CRACK_H
+#define CRACK_H
 
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <crypt.h>
+
+#define MD5	1
+#define SHA1	2
+
+// Define a cracing task and all attribus which related on this
+typedef struct {
+	int base;
+	int keysize_max;
+	char* charset;
+	char* hash;
+	char* salt;
+	int algorithm;
+
+	//additional information
+	unsigned long long int keyrange;
+
+	// for splitting calculation, e.g. threading or distributed computing
+	char* start_key;
+	char* end_key;
+} crack_task;
+
+unsigned long long int keyrange(crack_task);
+int keynr_2_key(crack_task, int, char*);
+int get_next_key(crack_task, char*, int);
+int ben_next_key(crack_task, char*);
+int compare_hash(char*, char*);
+
+#endif
